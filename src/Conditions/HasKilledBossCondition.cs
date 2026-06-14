7dc80015c4a39a8b4d9252329e5e39101c3a5a30
WarperSan <leumas.ecole@gmail.com>
2026-06-13 22:59:34 -0400

    Added condition

---
+++ src/Conditions/HasKilledBossCondition.cs
@@ -0,0 +1,21 @@
+using BingoAPI.Conditions;
+using Silksong.BingoSync.Data;
+using Silksong.BingoSync.Extensions;
+
+namespace Silksong.BingoSync.Conditions;
+
+/// <summary>
+/// Checks if the player has killed a given <see cref="Boss"/>
+/// </summary>
+internal sealed class HasKilledBossCondition : ICondition
+{
+	private readonly Boss _boss;
+
+	public HasKilledBossCondition(ConditionData data)
+	{
+		_boss = data.GetRequiredParameter<Boss>("boss");
+	}
+
+	/// <inheritdoc />
+	public bool IsMet() => PlayerData.instance.IsBossKilled(_boss);
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,6 @@
 using BepInEx;
+using BingoAPI.Conditions;
+using Silksong.BingoSync.Conditions;
 using Silksong.BingoSync.Helpers;
 
 namespace Silksong.BingoSync;
@@ -8,7 +10,14 @@ public partial class Plugin : BaseUnityPlugin
 {
 	private void Awake()
 	{
+		AddConditions();
+
 		Patch.ApplyAll();
 		Log.Info($"Plugin {Name} ({Id}) has loaded!");
+	}
+
+	private static void AddConditions()
+	{
+		ConditionRegistry.TryAdd("has_killed_boss", data => new HasKilledBossCondition(data));
 	}
 }
