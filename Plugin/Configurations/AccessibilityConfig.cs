26d158dcffb9695a1c04d85f9d5e9698076aa7fc
WarperSan <leumas.ecole@gmail.com>
2026-08-02 14:18:23 -0400

    Added empty `AccessiblityConfig`

---
+++ Plugin/Configurations/AccessibilityConfig.cs
@@ -0,0 +1,14 @@
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace Silksong.BingoSync.Configurations;
+
+/// <summary>
+/// Class that holds the configurations related to accessibility features
+/// </summary>
+internal class AccessibilityConfig
+{
+	private const string SECTION = "Accessibility";
+
+	public AccessibilityConfig(ConfigFile cfg) { }
+}
