2d5e92843e95ba541a9ef5b16029124b518045b0
WarperSan <leumas.ecole@gmail.com>
2026-08-02 10:12:08 -0400

    Added raw flea entries

--- Plugin/Data/Flea.cs
+++ Plugin/Data/Flea.cs
@@ -9,4 +9,36 @@ namespace Silksong.BingoSync.Data;
 // ReSharper disable IdentifierTypo
 // ReSharper disable StringLiteralTypo
 [JsonConverter(typeof(StringEnumConverter))]
-public enum Flea { }
+public enum Flea
+{
+	TheMarrow,
+	DeepDocks1,
+	DeepDocks2,
+	DeepDocks3,
+	HuntersMarch,
+	FarFields1,
+	FarFields2,
+	Wormways,
+	Greymoor1,
+	Greymoor2,
+	Greymoor3,
+	Bellhart,
+	Shellwood,
+	BlastedSteps,
+	SinnersRoad,
+	Bilewater1,
+	Bilewater2,
+	Bilewater3,
+	Underworks1,
+	Underworks2,
+	ChoralChambers1,
+	ChoralChambers2,
+	Memorium,
+	TheSlab1,
+	TheSlab2,
+	MountFay,
+	SandsOfKarak,
+	PutrifiedDucts,
+	WhisperingVaults1,
+	WhisperingVaults2,
+}
