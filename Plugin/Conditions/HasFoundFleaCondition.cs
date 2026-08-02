395bbb430cf908b31ecd2f8cddd7c872bf253069
WarperSan <leumas.ecole@gmail.com>
2026-08-02 10:13:43 -0400

    Added shallow `has_found_flea`

---
+++ Plugin/Conditions/HasFoundFleaCondition.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel;
+using BingoAPI.Conditions.Attributes;
+using BingoAPI.Conditions.Interfaces;
+using Newtonsoft.Json;
+using Silksong.BingoSync.Data;
+
+namespace Silksong.BingoSync.Conditions;
+
+[Condition("has_found_flea")]
+internal sealed class HasFoundFleaCondition : ICondition
+{
+	[JsonProperty("flea")]
+	[JsonRequired]
+	[Description("Name of the flea to find")]
+	public required Flea Flea { get; init; }
+
+	/// <inheritdoc />
+	public bool IsMet() => throw new NotImplementedException();
+}
