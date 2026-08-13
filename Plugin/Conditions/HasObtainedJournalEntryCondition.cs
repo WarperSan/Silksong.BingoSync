b5a8a85f9dced09ca35e03b6eb6df785c05bfeb0
WarperSan <78861227+WarperSan@users.noreply.github.com>
2026-08-13 12:05:48 -0400

    53 condition has obtained journal entry (#55)
    * Added empty `Data.Enemy`
    
    * Fixed type
    
    * Added enemy entries
    
    * Added basic `HasObtainedJournalEntryCondition`
    
    * Added `"enemy"` parameter
    
    * Added basic condition
    
    * Added temporary checks
    
    * Removed invalid entries
    
    * Linked entries with checks
    
    * Added missing entries for `HasJournalEntry(Boss)`
    
    * Fixed names
    
    * Revert internal method
    
    * Simplified `HasObtainedJournalEntry(Enemy)`

---
+++ Plugin/Conditions/HasObtainedJournalEntryCondition.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel;
+using BingoAPI.Conditions.Attributes;
+using BingoAPI.Conditions.Interfaces;
+using Newtonsoft.Json;
+using Silksong.BingoSync.Data;
+using Silksong.BingoSync.Extensions;
+
+namespace Silksong.BingoSync.Conditions;
+
+/// <summary>
+/// Checks if the player has obtained the journal entry of a given <see cref="Data.Enemy"/>
+/// </summary>
+[Condition("has_obtained_journal_entry")]
+internal sealed class HasObtainedJournalEntryCondition : ICondition
+{
+	[JsonProperty("enemy")]
+	[JsonRequired]
+	[Description("Name of the enemy to obtain the journal entry for")]
+	public required Enemy Enemy { get; init; }
+
+	/// <inheritdoc />
+	public bool IsMet() => PlayerData.instance.HasObtainedJournalEntry(Enemy);
+}
